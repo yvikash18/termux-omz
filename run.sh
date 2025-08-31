1b9bbd63f72fbd76be2ec889406e0add5cae099d
Vikash Yadav <yvikash880@gmail.com>
2025-08-31 12:32:35 +0530

    Remove unnecessary whitespace in run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
 ## Simple bootstrap script for termux-omz
-
 set -e  # exit on error
-
 echo "[*] Cloning repository..."
 git clone https://github.com/yvikash18/termux-omz.git
-
 echo "[*] Entering termux-omz directory..."
 cd termux-omz
-
 echo "[*] Making install script executable..."
 chmod +x install
-
 echo "[*] Running install script..."
 ./install
-
 echo "[✔] Done! oh-my-zsh should now be installed on your Termux."
